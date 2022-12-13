badd87664bc2134b299d83497449e41e9700d037
Michael Ho <michael.ho@ieee.org>
2022-12-13 06:24:24 +0100

    rsdml-lib.go: remove logging
    Drop the logging from rsdml-lib.go so that the module is not coupled to
    a logging framework. This makes it more reuseable.
    
    The errors are adjusted to share information as needed to reduce the
    need for logging statements.

--- rsdml-lib.go
+++ rsdml-lib.go
@@ -1,35 +1,30 @@
 package rsdml
 
 import "fmt"
-import "log"
 import "os"
 import "path/filepath"
 import "strings"
 import "sort"
 
 func UpdateDirectory(dir string) error {
-	log.Printf("UpdateDirectory(%s)", dir)
-
 	// Get current mtime of directory
 	dirInfo, err := os.Lstat(dir)
 	if err != nil {
-		log.Print(err)
-		return fmt.Errorf("Path cannot be accessed: %s", dir)
+		return fmt.Errorf("Path cannot be accessed: %s: %s", dir, err)
 	}
 	dir_mtime := dirInfo.ModTime()
 
 	// Find newest mtime in directory
 	entries, err := os.ReadDir(dir)
 	if err != nil {
-		log.Print(err)
-		return fmt.Errorf("Path cannot be accessed: %s", dir)
+		return fmt.Errorf("Path cannot be accessed: %s: %s", dir, err)
 	}
 
 	newest_mtime := dir_mtime
 	for _, entry := range entries {
 		info, err := entry.Info()
 		if err != nil {
-			log.Print(err)
+			continue
 		}
 		entry_mtime := info.ModTime()
 		if entry_mtime.After(newest_mtime) {
@@ -41,10 +36,8 @@ func UpdateDirectory(dir string) error {
 	if newest_mtime != dir_mtime {
 		err := os.Chtimes(dir, newest_mtime, newest_mtime)
 		if err != nil {
-			log.Print(err)
-			return fmt.Errorf("Path cannot be touched: %s", dir)
+			return fmt.Errorf("Path cannot be touched: %s: %s", dir, err)
 		}
-		log.Printf("Touching %s with date: %s", dir, newest_mtime)
 	}
 
 	return nil
@@ -53,11 +46,9 @@ func UpdateDirectory(dir string) error {
 func RecurseDirectory(root_dir string) error {
 	// Input Sanitisation
 	root_dir = filepath.Clean(root_dir)
-	log.Printf("RecurseDirectory(%s)\n", root_dir)
 	fileInfo, err := os.Stat(root_dir)
 	if err != nil {
-		log.Print(err)
-		return fmt.Errorf("Path is not a directory or cannot be accessed: %s", root_dir)
+		return fmt.Errorf("Path is not a directory or cannot be accessed: %s: %s", root_dir, err)
 	}
 
 	if ! fileInfo.IsDir() {
@@ -68,7 +59,6 @@ func RecurseDirectory(root_dir string) error {
 	var dirs []string
 	err = filepath.Walk(root_dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Print(err)
 			return nil
 		}
 
@@ -80,8 +70,7 @@ func RecurseDirectory(root_dir string) error {
 	})
 
 	if err != nil {
-		log.Print(err)
-		return fmt.Errorf("Cannot traverse path: %s", root_dir)
+		return fmt.Errorf("Cannot traverse path: %s: %s", root_dir, err)
 	}
 
 	// Sort dirs from bottom to top (deepest first)
@@ -96,7 +85,6 @@ func RecurseDirectory(root_dir string) error {
 	for _, subdir := range dirs {
 		err = UpdateDirectory(subdir)
 		if err != nil {
-			log.Print(err)
 			failures = 1
 		}
 	}
